537987581eef923bcdcb4c133699a5eebd5a2972
naga-mohan-teja <142543550+naga-mohan-teja@users.noreply.github.com>
2023-08-21 15:32:32 +0530

    Update calculate sum of a number.py

--- calculate sum of a number.py
+++ calculate sum of a number.py
@@ -3,3 +3,9 @@ digit_sum = 0
 for digit in str(num):
     digit_sum += int(digit)
 print(digit_sum)  # Output: 15
+#################################################
+n=78
+n=str(n)
+l=sum([ int(i) for i in n])
+print(l)
+#################################################
